8f2fa6c359a27bd1287f5317cb293273f81ee9fc
Hassaan.dev <hassaanishere@gmail.com>
2023-08-17 18:10:11 +0500

    tailwind project

---
+++ tailwindproject/src/App.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-700 text-white flex md:justify-center sm:justify-start justify-center items-center h-[100px] md:w-[300px] w-[500px] absolute lg:top-10 md:top-16 top-20 left-10 font-bold md:text-[26px]">
+        Hello World
+      </div>
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-2 gap-y-4">
+        <div className="h-[300px] w-full bg-[red]"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+      </div>
+
+      <div className="flex md:flex-col flex-row justify-center items-center">
+        <h1>hello world</h1>
+        <h1>hello world</h1>
+      </div>
+
+      <div className="grid lg:grid-rows-4 md:grid-rows-3 sm:grid-rows-2 grid-rows-1 gap-x-2 grid-flow-col gap-y-4">
+        <div className="h-[300px] w-full bg-[red]"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-[red]"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-[red]"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-[red]"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-[red]"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-[red]"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+        <div className="h-[300px] w-full bg-black"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
